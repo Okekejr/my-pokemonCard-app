677d7956a69849e71db72de4a6585cd6e0870b96
Emmanuel <85338878+Okekejr@users.noreply.github.com>
2024-03-10 20:02:24 -0600

    usingVercelpostgres

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,15 @@
-import { Pool } from "pg";
+import { db } from "@vercel/postgres";
 
-const database = new Pool({
-  user: process.env.NEXT_PUBLIC_DB_USER,
-  host: process.env.NEXT_PUBLIC_DB_HOST,
-  database: process.env.NEXT_PUBLIC_DB,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-});
+// const database = new Pool({
+//   user: process.env.NEXT_PUBLIC_DB_USER,
+//   host: process.env.NEXT_PUBLIC_DB_HOST,
+//   database: process.env.NEXT_PUBLIC_DB,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+// });
 
 export const query = async (text: string, params: any[] = []) => {
-  const client = await database.connect();
+  const client = await db.connect();
   try {
     const result = await client.query(text, params);
     return result.rows;
